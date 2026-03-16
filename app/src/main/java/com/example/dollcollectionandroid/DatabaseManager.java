2ab8a9843f5d6cf15b917749b413d41a739703c4
OmmePester <osmanapp12@gmail.com>
2026-03-16 21:42:12 +0300

    commit all v6, doing documentations.

--- app/src/main/java/com/example/dollcollectionandroid/DatabaseManager.java
+++ app/src/main/java/com/example/dollcollectionandroid/DatabaseManager.java
@@ -12,25 +12,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * This Class manages all processes related to our SQL DB 'closet.db'.
+ * It extends SQLiteOpenHelper to be able to execute all essential commands:
+ * CRUD (Create, Read, Update, Delete). It saves Doll data in 'closet.db', then
+ * applies changes and/or deletes it according to user's command.
  */
 
 public class DatabaseManager extends SQLiteOpenHelper {
 
-    // This tells Java where the file is (Android handles the path internally)
-    private static final String DATABASE_NAME = "closet.db";
-    // Version increased to 3 to add new columns (Date and Time), and change hint to gender
+    // VARIABLES
+    // Version increased to 3 after adding new columns (Date and Time), and changing hint to gender!!!!
     private static final int DATABASE_VERSION = 3;
     private Context myContext;
 
     // CONSTRUCTOR
     public DatabaseManager(Context context) {
-    // We call StorageHelper to get the path in the hidden .closetDollUp folder
+        // calls StorageHelper to get the path to hidden '.closetDollUp' folder
         super(context, StorageHelper.getDatabasePath(context), null, DATABASE_VERSION);
         this.myContext = context; // Kept intact as requested!
     }
 
     @Override
+    // this startup method initializes DatabaseManager
     public void onCreate(SQLiteDatabase db) {
         // STRICT ORDER: ID, Path, Name, Brand, Model, Year, Desc, Gender, Date, Time
         // ALL COLUMNS ARE NOT NULL
@@ -49,14 +52,18 @@ public class DatabaseManager extends SQLiteOpenHelper {
     }
 
     @Override
+    // this method handles database structure changes by updating it
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS items");
         onCreate(db);
     }
 
-    // This method inserts doll for the first time, to get int ID
+    // this method inserts Doll data (name and image path) for the first time, and returns unique ID
     public int addDoll(String name, String path) {
+        // creates and opens SQL DB, for writing in it
         SQLiteDatabase db = this.getWritableDatabase();
+
+        // instantiates and packs variables into ContentValues
         ContentValues values = new ContentValues();
         // the actual two values name and image_path
         values.put("image_path", path);
@@ -70,24 +77,33 @@ public class DatabaseManager extends SQLiteOpenHelper {
         values.put("birth_date", "");
         values.put("birth_time", "");
 
+        // calls for, stores, and returns ID (autoincrement)
         long id = db.insert("items", null, values);
-        return (int) id; // returns the ID (autoincrement)
+        return (int) id;
     }
 
-    // This method might e a bit redundant now
+    // this method completes Doll data save by updating Image Path with correct and final file name (based on autoincrement ID)
     public void completeDollInitialSave(int id, String fileName) {
+        // creates and opens SQL DB, for writing in it
         SQLiteDatabase db = this.getWritableDatabase();
+
+        // instantiates ContentValues and packs variable "image_path" into it
         ContentValues values = new ContentValues();
         values.put("image_path", fileName);
+
+        // executes SQL UPDATE command on "items" table
         db.update("items", values, "id = ?", new String[]{String.valueOf(id)});
     }
 
     // this method interacts with a Dolls by ID from SQL DB, runs in DollDetailActivity
     public Doll getDollById(int id) {
+        // creates and opens SQL DB, for reading from it
         SQLiteDatabase db = this.getReadableDatabase();
-        // Get all dolls from the 'closet' database [cite: 2026-02-22]
+
+        // reads specific Doll by given ID from "items" table and saves it in Curser object
         Cursor cursor = db.query("items", null, "id=?", new String[]{String.valueOf(id)}, null, null, null);
 
+        // creates and returns Doll object, using cursorToDoll(Cursor) method
         if (cursor != null && cursor.moveToFirst()) {
             Doll doll = cursorToDoll(cursor);
             cursor.close();
@@ -96,13 +112,21 @@ public class DatabaseManager extends SQLiteOpenHelper {
         return null;
     }
 
-    // this method creates List of Dolls by loading them from SQL DB, runs in CollectionActivity
+    // this method creates List of Dolls by loading them from SQL DB, runs in CollectionActivity (and SettingsActivity)
     public List<Doll> getAllDolls() {
+        // creates empty List
         List<Doll> dolls = new ArrayList<>();
+
+        // prepares SQL query to read everything
         String sql = "SELECT * FROM items";
+
+        // creates and opens SQL DB, for reading from it
         SQLiteDatabase db = this.getReadableDatabase();
+
+        // runs query on our DB and saves in Curser object
         Cursor cursor = db.rawQuery(sql, null);
 
+        // moves Curser one by one, adds all Dolls to List, and closes Curser
         if (cursor.moveToFirst()) {
             do {
                 dolls.add(cursorToDoll(cursor));
@@ -112,7 +136,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
         return dolls;
     }
 
-    // this is a HELPER method to MAP cursor to Doll objects
+    // this helper method maps cursor row data to newly instantiated Doll object's variables
     private Doll cursorToDoll(Cursor cursor) {
         return new Doll(
                 cursor.getInt(cursor.getColumnIndexOrThrow("id")),
@@ -128,10 +152,13 @@ public class DatabaseManager extends SQLiteOpenHelper {
         );
     }
 
-    // this method updates Doll variables, which runs in DollDetailActivity
+    // this method updates Doll variables, it is called in DollDetailActivity
     public void updateFullDollDetails(int id, String path, String name, String brand, String model, int year, String desc,
                                       String gender, String bDate, String bTime) {
+        // creates and opens SQL DB, for writing in it
         SQLiteDatabase db = this.getWritableDatabase();
+
+        // instantiates and packs variables into ContentValues
         ContentValues values = new ContentValues();
         values.put("image_path", path);
         values.put("name", name);
@@ -143,12 +170,16 @@ public class DatabaseManager extends SQLiteOpenHelper {
         values.put("birth_date", bDate);
         values.put("birth_time", bTime);
 
+        // executes SQL UPDATE command on "items" table
         db.update("items", values, "id = ?", new String[]{String.valueOf(id)});
     }
 
-    // This method deleting Doll by ID
+    // This method deletes a Doll by its ID from our "items" table, it is called in DollDetailActivity
     public void deleteDollById(int id) {
+        // creates and opens SQL DB
         SQLiteDatabase db = this.getWritableDatabase();
+
+        // executes SQL DELETE command on "items" table
         db.delete("items", "id = ?", new String[]{String.valueOf(id)});
     }
 }
